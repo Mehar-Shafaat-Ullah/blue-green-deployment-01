5d723aaa18e3a03da659affe887a501e8995c91b
Mehar Shafaat Ullah <142030619+Mehar-Shafaat-Ullah@users.noreply.github.com>
2025-09-02 22:07:23 +0500

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const port = 3000;
-const version = process.env.VERSION || "blue"; // identify blue/green
+const version = process.env.VERSION || "green"; // identify blue/green
 
 app.get("/", (req, res) => {
   res.send(`Hello from ${version} version!`);
